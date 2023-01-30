e4ef9f9fac84647b0cfcd560ddce6635cd9cc9dd
dominionthings <dominionthings@gmail.com>
2023-01-30 10:38:39 +0300

    First from local

---
+++ assets/js/footer.js
@@ -0,0 +1,62 @@
+var footing =
+'<footer class="footer">\
+        <div class="container">\
+            <div class="row">\
+                <div class="col-md-4 col-sm-12">\
+                    <h2>About Us</h2>\
+                    <p>\
+                        Started on 7th December 2014, The Seeing Eyes (TSE) is committed to fighting preventable blindness. We have treated over 50,000 Ugandans and beyond with eye conditions under our community outreach and corporate partnerships. Be apart of this move to have sight recovered in Africa’s hard to reach places.\
+                    </p>\
+                    <p>We focus on technologies that promise to reduce costs, streamline processes and speed time-to-market, Backed by our strong quality processes and rich experience managing global... </p>\
+                </div>\
+                <div class="col-md-4 col-sm-12">\
+                    <h2>Useful Links</h2>\
+                    <ul class="list-unstyled link-list">\
+                        <li><a ui-sref="about" href="about_us.html">About us</a><i class="fa fa-angle-right"></i></li>\
+                        <!-- <li><a ui-sref="portfolio" href="#/portfolio">Portfolio</a><i class="fa fa-angle-right"></i></li> -->\
+                        <li><a ui-sref="projects" href="projects.html">Projects</a><i class="fa fa-angle-right"></i></li>\
+                        <li><a ui-sref="gallery" href="gallery.html">Gallery</a><i class="fa fa-angle-right"></i></li>\
+                        <li><a ui-sref="contact" href="contact_us.html">Contact us</a><i class="fa fa-angle-right"></i></li>\
+                    </ul>\
+                </div>\
+                <div class="col-md-4 col-sm-12 map-img">\
+                    <h2>Contact Us</h2>\
+                    <address class="md-margin-bottom-40">\
+                        Entebbe Central Market <br>\
+                        Opposite Shell <br>\
+                        Kampala Road <br>\
+                        Phone: +256 776 233241  <br>\
+                        Email: <a href="mailto:info@anybiz.com" class="">info@theseeingeyes.org</a><br>\
+                        Web: <a href="smart-eye.html" class="">www.theseeingeyes.org</a>\
+                    </address>\
+                </div>\
+            </div>\
+            <div class="nav-box row clearfix">\
+                <div class="inner col-md-9 clearfix">\
+                    <ul class="footer-nav clearfix">\
+                        <li><a href="index.html">Home</a></li>\
+                        <li><a href="about_us.html">About</a></li>\
+                        <li><a href="gallery.html">Gallery</a></li>\
+                        <li><a href="partners.html">Partners</a></li>\
+                        <li><a href="projects.html">Projects</a></li>\
+                        <li><a href="contact_us.html">Contact</a></li>\
+                    </ul>\
+                </div>\
+                  <div class="donate-link col-md-3"><a href="contact_us.html" class="btn btn-primary "><span class="btn-title">Contact Us Now</span></a></div>\
+            </div>\
+        </div>\
+    </footer>\
+    <div class="copy">\
+        <div class="container">\
+            <a href="https://www.theseeingeyes.org">2021 &copy; All Rights Reserved | Designed and Developed for The Seeing Eyes</a>\
+            <span>\
+                <!-- <a><i class="fab fa-github"></i></a> -->\
+                <a><i class="fab fa-google-plus-g"></i></a>\
+                <!-- <a><i class="fab fa-pinterest-p"></i></a> -->\
+                <a><i class="fab fa-twitter"></i></a>\
+                <a><i class="fab fa-facebook-f"></i></a>\
+            </span>\
+        </div>\
+    </div>';
+
+document.getElementById('foot').innerHTML = footing;
